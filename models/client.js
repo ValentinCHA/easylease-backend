60fb1697f82a42b03af5312f929bedd5319f7c94
Nicolas <nicolas99@hotmail.fr>
2022-12-13 20:34:31 +0100

    nico back fin de journée

--- models/client.js
+++ models/client.js
@@ -1,21 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const clientSchema = mongoose.Schema({
-  
   name: String,
-<<<<<<< HEAD
-  adress: String,
-  EmployedSince: Number,
-  ClientBirth: Number,
-  chiffre: Number,
-  interlocutor: { type: mongoose.Schema.Types.ObjectId, ref: 'interlocutors' },
-  scenary: { type: mongoose.Schema.Types.ObjectId, ref: 'scenary' },
-  contrat:{ type: mongoose.Schema.Types.ObjectId, ref: 'contrats' },
-
-});
-
-const Client = mongoose.model('client', clientSchema);
-=======
   address: String,
   numberOfEmployees: Number,
   clientBirth: Date,
@@ -23,10 +9,8 @@ const Client = mongoose.model('client', clientSchema);
   // interlocutor: { type: mongoose.Schema.Types.ObjectId, ref: 'interlocutors' },
   // scenary: { type: mongoose.Schema.Types.ObjectId, ref: 'scenary' },
   // contrat:{ type: mongoose.Schema.Types.ObjectId, ref: 'contrats' },
-
 });
 
-const Client = mongoose.model('clients', clientSchema);
->>>>>>> cd3e22b93a2f907ef74e046809181af631a65a78
+const Client = mongoose.model("clients", clientSchema);
 
 module.exports = Client;
