f7999fdc70af4db5a15022a2b06db10b71ef15bc
Valentin Charpentier <valentincharpentier@hotmail.com>
2022-12-13 17:48:06 +0100

    route scenario finie VAL

--- models/client.js
+++ models/client.js
@@ -3,19 +3,6 @@ const mongoose = require('mongoose');
 const clientSchema = mongoose.Schema({
   
   name: String,
-<<<<<<< HEAD
-  adress: String,
-  EmployedSince: Number,
-  ClientBirth: Number,
-  chiffre: Number,
-  interlocutor: { type: mongoose.Schema.Types.ObjectId, ref: 'interlocutors' },
-  scenary: { type: mongoose.Schema.Types.ObjectId, ref: 'scenary' },
-  contrat:{ type: mongoose.Schema.Types.ObjectId, ref: 'contrats' },
-
-});
-
-const Client = mongoose.model('client', clientSchema);
-=======
   address: String,
   numberOfEmployees: Number,
   clientBirth: Date,
@@ -27,6 +14,5 @@ const Client = mongoose.model('client', clientSchema);
 });
 
 const Client = mongoose.model('clients', clientSchema);
->>>>>>> cd3e22b93a2f907ef74e046809181af631a65a78
 
 module.exports = Client;
